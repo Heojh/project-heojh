d05a2c18f79e3158b160469f34937e572aa84dc3
heojh <jihaeng1989@gmail.com>
2021-05-02 22:44:12 +0900

    환자 정보 요청 및 응답 데이터 확인

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { URL } from "./config/config";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    // 환자 정보 요청 및 응답 데이터 확인
+    axios.get(`${URL}/patient/list`).then((response) => {
+      if (response.data) {
+        console.log(response.data);
+      } else {
+        alert("데이터를 가져오는데 실패했습니다.");
+      }
+    });
+  }, []);
+
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
